421939df55ae9389ffb018372075eeb39b3350b8
yannafaith <yannafaith97@gmail.com>
2019-04-25 18:39:19 -0700

    adds mockData

--- dev/src/App.js
+++ dev/src/App.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
 
+let mockPosts = [
+  {
+  "creator_id": 1,
+  "title": "mock title",
+  "body": "mock post",
+  "created_at": "timestamp value",
+  "delete_at": "day and time of automatic deletion"
+  },
+  {
+    "creator_id": 1,
+    "title": "mock title",
+    "body": "mock post",
+    "created_at": "timestamp value",
+    "delete_at": "day and time of automatic deletion"
+  },
+]
+
+let mockUsers = [
+  {
+    "id": 1,
+    "username": "mock username",
+    "password": "mock password",
+    "email": "mock email",
+  },
+  {
+    "id": "user id",
+    "username": "mock username",
+    "password": "mock password",
+    "email": "mock email",
+  },
+]
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h2>Happy Hacking!</h2>
+      <div>
+        {mockPosts.map(post => {
+          return (
+            <div> <p>
+              {post.title} <br/>
+              {post.body} <br/>
+              {post.created_at} <br/>
+              {post.delete_at} <br/>
+            </p> </div> 
+          )
+        })}
       </div>
     );
   }
